9998a2934bde586ba968911f3d8643b83fac4b65
Maksym <PTNPNH722@gmail.com>
2022-10-30 17:31:29 +0200

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const playerVimeo = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
-const currentTime = localStorage.getItem(storage_key);
+const currentTime = localStorage.getItem(STORAGE_KEY);
 
 if (currentTime) {
     playerVimeo.setCurrentTime(currentTime);
